d84a6baa1a2a373764319583efef957298ada99e
Brianna Townsend <p4r4digm@gmail.com>
2023-07-16 09:39:34 -0700

    more fixes for use externally

--- libft2/libft2.h
+++ libft2/libft2.h
@@ -1,6 +1,47 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef _WIN32
 #include <wchar.h>
+#endif
+
+#ifdef _WIN32
+
+// Windows
+typedef wchar_t UNICHAR;
+#define UNICHAR_STRCPY(a, b) wcscpy(a, b)
+#define UNICHAR_STRNCPY(a, b, c) wcsncpy(a, b, c)
+#define UNICHAR_STRCMP(a, b) wcscmp(a, b)
+#define UNICHAR_STRNCMP(a, b, c) wcsncmp(a, b, c)
+#define UNICHAR_STRCAT(a, b) wcscat(a, b)
+#define UNICHAR_STRDUP(a) _wcsdup(a)
+#define UNICHAR_FOPEN(a, b) _wfopen(a, L ## b)
+#define UNICHAR_CHDIR(a) _wchdir(a)
+#define UNICHAR_GETCWD(a, b) _wgetcwd(a, b)
+#define UNICHAR_RENAME(a, b) _wrename(a, b)
+#define UNICHAR_REMOVE(a) _wremove(a)
+#define UNICHAR_STRLEN(a) wcslen(a)
+#else
+
+// other OSes
+typedef char UNICHAR;
+#define UNICHAR_STRCPY(a, b) strcpy(a, b)
+#define UNICHAR_STRNCPY(a, b, c) strncpy(a, b, c)
+#define UNICHAR_STRCMP(a, b) strcmp(a, b)
+#define UNICHAR_STRNCMP(a, b, c) strncmp(a, b, c)
+#define UNICHAR_STRCAT(a, b) strcat(a, b)
+#define UNICHAR_STRDUP(a) strdup(a)
+#define UNICHAR_FOPEN(a, b) fopen(a, b)
+#define UNICHAR_CHDIR(a) chdir(a)
+#define UNICHAR_GETCWD(a, b) getcwd(a, b)
+#define UNICHAR_RENAME(a, b) rename(a, b)
+#define UNICHAR_REMOVE(a) remove(a)
+#define UNICHAR_STRLEN(a) strlen(a)
+#endif
+
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -62,7 +103,7 @@ extern song_t song;
 extern instr_t* instr[128 + 4];
 extern note_t* pattern[MAX_PATTERNS];
 
-int libft2_loadModule(wchar_t* path);
+int libft2_loadModule(UNICHAR* path);
 void libft2_unloadModule();
 
 // EXTREMELY LOW-LEVEL ACCESS DO NOT TOUCH
@@ -77,3 +118,7 @@ instr_t*** libft2_getInstruments();
 
 // note_t *pattern[MAX_PATTERNS];
 note_t*** libft2_getPatterns();
+
+#ifdef __cplusplus
+}
+#endif
