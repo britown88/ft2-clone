65757b8710c45bc4ac6959a0524cd8c4630d42fd
Brianna Townsend <p4r4digm@gmail.com>
2023-07-16 09:52:51 -0700

    license update

--- vs2019_project/libft2-test/main.c
+++ vs2019_project/libft2-test/main.c
@@ -3,13 +3,7 @@
 int main(int argc, char* argv[]) {
    //if (!libft2_startup()) {
       if (!libft2_loadModule(L"D:/Downloads/Arpeggio.xm")) {
-         //int i = 'fuck';
-         //i++;
-         //song_t* song = libft2_getSong();
-         //int16_t** numRows = libft2_getPatternNumRows();
-         //instr_t*** instruments = libft2_getInstruments();
-         //note_t*** patterns = libft2_getPatterns();
-         // 
+
          song;
          instr;
          pattern;
@@ -22,11 +16,7 @@ int main(int argc, char* argv[]) {
       if (!libft2_loadModule(L"D:/Downloads/Arpeggio.xm")) {
          int i = 'fuck';
          i++;
-         song_t* song = libft2_getSong();
-         int16_t** numRows = libft2_getPatternNumRows();
-         instr_t*** instruments = libft2_getInstruments();
-         note_t*** patterns = libft2_getPatterns();
-         //patterns = 0;
+
          libft2_unloadModule();
       }
       //libft2_shutdown();
